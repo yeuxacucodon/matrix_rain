c19770eb63a836c78c5dfec1c6574cdd18d18266
Vo Quang Chien <chiendien2006@gmail.com>
2023-03-04 12:50:52 +0700

    update

--- matrix.js
+++ matrix.js
@@ -1,18 +1,16 @@
-const canvas = document.getElementById('canvas')
-const context = canvas.getContext('2d')
+const canvas = document.getElementById("canvas")
+const context = canvas.getContext("2d")
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-let charArr = ['1', '0']
+let charArr = ["1", "0"]
 
 let maxCharCount = 300
 let fallingCharArr = []
-let fontSize = 13
+let fontSize = 14
 let maxColumns = window.innerWidth / fontSize
-
 let frames = 0
-let off
 
 function randomRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
@@ -27,7 +25,7 @@ class FallingChar {
   draw(context) {
     this.value = charArr[randomRange(0, charArr.length - 1)]
     this.speed = (Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 10
-    context.fillStyle = '#00FF00'
+    context.fillStyle = "#00FF00"
     context.font = `${fontSize}px sans-serif`
     context.fillText(this.value, this.x, this.y)
     this.y += this.speed
@@ -48,7 +46,7 @@ function update() {
     )
     fallingCharArr.push(fallingChar)
   }
-  context.fillStyle = 'rgba(0,0,0,0.09)'
+  context.fillStyle = "rgba(0,0,0,0.05)"
   context.fillRect(0, 0, window.innerWidth, window.innerHeight)
   for (let i = 0; i < fallingCharArr.length && frames % 2 === 0; i++) {
     fallingCharArr[i].draw(context)
